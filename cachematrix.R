54ea8fa5f0fa393694895100baab6f7f5aaff116
Gabelytics <gabe.analytics@gmail.com>
2018-03-19 20:52:41 +1100

    pushing prog ass 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following program is designed to take a Matrix and compute the Matrix's inverse. If the inverse of a matrix has already been computed,
+#the inverse will be taken from cache. If not, a new matrix inverse will be computed.
 
-## Write a short comment describing this function
+
+#This function takes a Matrix and outputs functions which do the following:
+## set the matrix
+## get the matrix
+## set the inverse of the matrix
+## get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
+#This function checks will check if the inverse of the matrix has been calculated, if yes, the function will get the inverse matrix from cache
+#if not, the matrix will compute the inverse of the matrix via the setinverse function.
+
+cacheinverse <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
